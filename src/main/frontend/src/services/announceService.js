34ace31d693d6b8191bd283ea24b513ea686a29c
Chaeyoon-k <codbs9265@gmail.com>
2025-05-21 13:58:29 +0900

    modi

--- src/main/frontend/src/services/announceService.js
+++ src/main/frontend/src/services/announceService.js
@@ -1,7 +1,7 @@
 // services/announceService.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8485/api';
+const API_BASE_URL = 'https://recall-final-backendtest.onrender.com/api';
 
  // 공지사항전체
 export const fetchAnnouncements = async (params) => {
