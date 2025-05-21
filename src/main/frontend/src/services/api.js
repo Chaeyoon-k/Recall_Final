821cc51d3c7d51d6ec60671c0cd030ef5e7eaed3
Chaeyoon-k <codbs9265@gmail.com>
2025-05-21 11:40:59 +0900

    front modify

--- src/main/frontend/src/services/api.js
+++ src/main/frontend/src/services/api.js
@@ -1,9 +1,10 @@
 // services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://recall-final-backend.onrender.com/api'; // 예시 백엔드 URL
+const API_BASE_URL = 'https://recall-final-backendtest.onrender.com/api'; // 예시 백엔드 URL
 
 export const fetchTestData = async () => {
+  console.log("axios 호출 시작");
   try {
     // const response = await axios.get(`${API_BASE_URL}/test`);
     const response = await axios.get(`${API_BASE_URL}/test`);
